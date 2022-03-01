1085d709aa5f32c635609264deb1791a13aba0be
Ankit Tripathy <ankittripathy1306@gmail.com>
2022-03-01 18:17:37 +0530

    Fixed security issues

--- app.js
+++ app.js
@@ -10,26 +10,43 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const localStrategy = require("passport-local");
 const path = require("path");
+const helmet = require("helmet");
+const mongoStore = require("connect-mongo");
 const mongoose = require("mongoose");
 const ExpressError = require("./utils/ExpressError");
+// const mongoDBStore = require("connect-mongo")(session);
 const campgroundsRoutes = require("./routes/campgrounds");
 const reviewsRoutes = require("./routes/reviews");
 const userRoutes = require("./routes/users");
 const User = require("./models/user");
 const app = express();
+// const dbUrl = process.env.DB_URL;
+
+const dbUrl = "mongodb://localhost:27017/yelp-camp";
 
 mongoose
-    .connect("mongodb://localhost:27017/yelp-camp", {
+    .connect(dbUrl, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
     .then(() => console.log("Connected to Mongo Successfully"))
     .catch((e) => console.log("Could not connect to Mongo", e));
 
+const store = mongoStore.create({
+    mongoUrl: "mongodb://localhost:27017/yelp-camp",
+    crypto: { secret: "dontknowityet" },
+    touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", function (e) {
+    console.log("Session Store Error", e);
+});
+
 const sessionConfig = {
     secret: "thisIsASecret",
     resave: false,
     saveUninitialized: true,
+    store,
     cookie: {
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
@@ -42,6 +59,51 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(session(sessionConfig));
 app.use(flash());
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                `https://res.cloudinary.com/ankit1306-cloud/`, //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
